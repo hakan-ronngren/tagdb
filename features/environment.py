9cbea630635b206e8a2ff9621eacacd68aaf98d6
Håkan Rönngren <hakan.ronngren@gmail.com>
2018-09-01 10:20:36 +0200

    shut down a conflicting server before all tests

--- features/environment.py
+++ features/environment.py
@@ -2,6 +2,19 @@ import http.client
 import glob, os, time
 
 def before_all(context):
+    # If there is a server on this port, shut it down
+    try:
+        http.client.HTTPConnection('localhost:3134').request('POST', '/shutdown')
+        while True:
+            try:
+                http.client.HTTPConnection('localhost:3134').request('GET', '/')
+                time.sleep(0.1)
+            except ConnectionRefusedError as e:
+                break
+    except ConnectionRefusedError as e:
+        pass
+
+    # Start a server in the test data directory
     os.system('cd features/testdata && ( ../../tagdb-server.py > testserver.log 2>&1 & ) &')
     while True:
         try:
